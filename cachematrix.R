2290a544f965f27ea75beac01e8e7e58d67c5928
pandaking <57734829+johanftg@users.noreply.github.com>
2021-06-26 21:10:42 -0500

    Update cachematriz.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,36 @@
 
 ## Write a short comment describing this function
 
+## Función util que establece la matriz y la inversa
 makeCacheMatrix <- function(x = matrix()) {
-
+  inversa <- NULL
+  set <- function(y) {
+    x <<- y
+    inversa <<- NULL
+  }
+  get <- function() {x}
+  setInversa <- function(inversa1) {inversa <<- inversa1}
+  getInversa <- function() {inversa}
+  list(set = set, get = get,
+       setInversa = setInversa,
+       getInversa = getInversa)
 }
-
 
 ## Write a short comment describing this function
 
+## Calcular y almacenar en cache la inversa de una matriz
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inversa <- x$getInversa()
+  if(!is.null(inversa)) {
+    message("metodo cache para matrix inversa")
+    return(inversa)
+  }
+  data <- x$get()
+  inversa <- solve(data, ...)
+  x$setInversa(inversa)
+  inversa
+  ## Return a matrix that is the inverse of 'x'
+
 }
+
+
